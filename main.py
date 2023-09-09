ceb9bbb8ad65105cfe5d5b9dc62b2bc13acbb697
saketlad75 <saket.lad@spit.ac.in>
2023-09-09 19:19:10 +0530

    Registration Page

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-
+@app.route('/register')
+def registration():
+    return render_template('register.html')
 if __name__ == '__main__':
     app.run(debug=True)
