a2eaad993191368c04caca34152ac69d8e6c875e
saketlad75 <saket.lad@spit.ac.in>
2023-09-09 17:52:06 +0530

    Trial dashboard

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/dashboard')
+def dash():
+    return render_template('dash.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
