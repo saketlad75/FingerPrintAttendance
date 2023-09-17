696e65480d73d8f402e746a5d0ff1c65264bcaea
saketlad75 <saket.lad@spit.ac.in>
2023-09-17 19:08:14 +0530

    Database problem solved
    Whenever the user entered the data into the registration page null values were being stored in the DB
    This issue is resolved now.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template,request
 import mysql.connector
-conn = mysql.connector.connect(host="127.0.0.1",user="root",password="@Svlad2305",database="user",auth_plugin='mysql_native_password')
+conn = mysql.connector.connect(host="127.0.0.1",
+                               user="root",
+                               password="@Svlad2305",
+                               database="dept",
+                               auth_plugin='mysql_native_password')
 cursor = conn.cursor()
 app = Flask(__name__)
 
@@ -12,8 +16,8 @@ def register():
     password = request.form.get('password')
     branch = request.form.get('branch')
 
-    query = 'INSERT INTO faculty VALUES(name,faculty_ID,email,password,branch)'
-    cursor.execute(query)
+    query = "INSERT INTO faculty (name,faculty_ID,email,password,branch) VALUES(%s, %s, %s, %s, %s)"
+    cursor.execute(query,(name, faculty_ID, email, password, branch))
     conn.commit()
     return "User registered Successfully"
 @app.route('/login_validation',methods=['POST'])
@@ -33,8 +37,22 @@ def login_validation():
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/register')
+@app.route('/register',methods=['POST','GET'])
 def registration():
-    return render_template('register.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        faculty_ID = request.form.get('faculty_id')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        branch = request.form.get('branch')
+
+        # Insert the user data into the 'faculty' table
+        query = "INSERT INTO faculty (name, faculty_ID, email, password, branch) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (name, faculty_ID, email, password, branch))
+        conn.commit()
+        return "User registered successfully"
+    else:
+        return render_template('register.html')
+    #return render_template('register.html')
 if __name__ == '__main__':
     app.run(debug=True)
